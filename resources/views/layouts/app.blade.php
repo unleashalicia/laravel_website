c3a219fe1100a397415e0dbe8e8e44a122719578
unleashalicia <alicia.adams.evans@gmail.com>
2018-07-06 21:56:05 -0700

    finished messages controller

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,13 +15,7 @@
         @endif
         <div class="row">
             <div class="col-md-8 col-lg-8">
-                @if(count($errors) > 0)
-                    @foreach($errors->all() as $error)
-                        <div class="alert alert-danger">
-                            {{$error}}
-                        </div>
-                    @endforeach
-                @endif
+                @include('inc.messages')
                 @yield('content')
             </div>
             <div class="col-md-4 col-lg-4">

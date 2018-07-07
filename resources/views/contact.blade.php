3b4dfcd997b3307d40105c389c5d7241cc618f90
unleashalicia <alicia.adams.evans@gmail.com>
2018-07-06 21:23:11 -0700

    Form inputs added

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,4 +2,14 @@
 
 @section('content')
     <h1>Contact</h1>
+    {!! Form::open(['url' => 'contact/sumbit']) !!}
+        <div class="form-group">
+            {{Form::label('name', 'Name')}}
+            {{Form::text('name', 'Enter Name')}}
+        </div>
+        <div class="form-group">
+            {{Form::label('email', 'E-Mail Address')}}
+            {{Form::text('email', 'example@gmail.com')}}
+        </div>
+    {!! Form::close() !!}
 @endsection
